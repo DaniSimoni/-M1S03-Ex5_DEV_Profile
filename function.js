ac1b55faec3cb190ca2e6eea02b0b966198e241d
Daniel Simoni Wegher <w.daniels@hotmail.com>
2023-02-21 17:36:13 -0300

    add input

--- function.js
+++ function.js
@@ -4,8 +4,23 @@ https://api.github.com/users/DaniSimoni */
 
 
 async function search () {
-    const response = await fetch('https://api.github.com/users/DaniSimoni')
-    const data = await response.json()
+
+    const formData = document.querySelector('#formData');
+    formData.addEventListener('submit', async (event) => {
+      event.preventDefault();
+      const name = document.querySelector('#nameAdd').value;
+          console.log(name)
+
+
+      if (!(name)) {
+        console.log('Digite o Nome como no Github');
+        return;
+    
+      }
+  
+      const response = await fetch(`https://api.github.com/users/${name}`)
+      const data = await response.json()
+
     console.log(data)
     document.getElementById('name').innerHTML = data?.name;
     document.getElementById('html_url').innerHTML = data?.html_url ;
@@ -13,6 +28,7 @@ async function search () {
     document.getElementById('location').innerHTML = data?.location;
     document.getElementById('avatar_url').src = data?.avatar_url; 
         console.log(data.avatar_url.value)
+});
 }
 search()
 
